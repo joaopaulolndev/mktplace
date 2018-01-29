8ea20ca7cca3da3ae49ca2103950b54314ec725b
João Paulo <joaopauloln7@gmail.com>
2018-01-28 23:33:26 -0200

    Create Category Model

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from portal.models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('name', )}
+    list_filter = ['hidden']
+    list_display = ("id", "name", "parent", "hidden")
+
+
+admin.site.register(Category, CategoryAdmin)
--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    parent = models.ForeignKey('Category', null=True, blank=True, related_name='cat_child', on_delete=False)
+    order = models.IntegerField(null=True, blank=True)
+    hidden = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        verbose_name = 'Category'
+
+    def __str__(self):
+        return self.name
